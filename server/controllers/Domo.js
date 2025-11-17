4b11d02d2d7ad0577c1c9ada9bb07afafcc724d4
chloe moon <cem6919@rit.edu>
2025-11-16 19:37:35 -0500

    added eslintrc for proper testing

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -1,12 +1,12 @@
-const models = require("../models");
+const models = require('../models');
 
 const { Domo } = models;
 
-const makerPage = (req, res) => res.render("app");
+const makerPage = (req, res) => res.render('app');
 
 const makeDomo = async (req, res) => {
   if (!req.body.name || !req.body.age) {
-    return res.status(400).json({ error: "Both name and age are required!" });
+    return res.status(400).json({ error: 'Both name and age are required!' });
   }
 
   const domoData = {
@@ -23,22 +23,22 @@ const makeDomo = async (req, res) => {
     console.log(err);
 
     if (err.code === 11000) {
-      return res.status(400).json({ error: "Domo already exists." });
+      return res.status(400).json({ error: 'Domo already exists.' });
     }
 
-    return res.status(400).json({ error: "An error occurred making domo!" });
+    return res.status(400).json({ error: 'An error occurred making domo!' });
   }
 };
 
 const getDomos = async (req, res) => {
   try {
     const query = { owner: req.session.account._id };
-    const docs = await Domo.find(query).select("name age").lean().exec();
+    const docs = await Domo.find(query).select('name age').lean().exec();
 
     return res.json({ domos: docs });
   } catch (err) {
     console.log(err);
-    return res.status(500).json({ error: "Error retrieving domos!" });
+    return res.status(500).json({ error: 'Error retrieving domos!' });
   }
 };
 
--- server/router.js
+++ server/router.js
@@ -1,39 +1,39 @@
-const controllers = require("./controllers");
-const mid = require("./middleware");
+const controllers = require('./controllers');
+const mid = require('./middleware');
 
 const router = (app) => {
-  app.get("/getDomos", mid.requiresLogin, controllers.Domo.getDomos);
+  app.get('/getDomos', mid.requiresLogin, controllers.Domo.getDomos);
 
   app.get(
-    "/login",
+    '/login',
     mid.requiresSecure,
     mid.requiresLogout,
-    controllers.Account.loginPage
+    controllers.Account.loginPage,
   );
   app.post(
-    "/login",
+    '/login',
     mid.requiresSecure,
     mid.requiresLogout,
-    controllers.Account.login
+    controllers.Account.login,
   );
 
   app.post(
-    "/signup",
+    '/signup',
     mid.requiresSecure,
     mid.requiresLogout,
-    controllers.Account.signup
+    controllers.Account.signup,
   );
 
-  app.get("/logout", mid.requiresLogin, controllers.Account.logout);
+  app.get('/logout', mid.requiresLogin, controllers.Account.logout);
 
-  app.get("/maker", mid.requiresLogin, controllers.Domo.makerPage);
-  app.post("/maker", mid.requiresLogin, controllers.Domo.makeDomo);
+  app.get('/maker', mid.requiresLogin, controllers.Domo.makerPage);
+  app.post('/maker', mid.requiresLogin, controllers.Domo.makeDomo);
 
   app.get(
-    "/",
+    '/',
     mid.requiresSecure,
     mid.requiresLogout,
-    controllers.Account.loginPage
+    controllers.Account.loginPage,
   );
 };
 
