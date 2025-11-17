04faf706ffc78131dd8a677f007df3d6e2e9eed9
chloe moon <cem6919@rit.edu>
2025-11-16 19:17:51 -0500

    domo d complete!

--- server/app.js
+++ server/app.js
@@ -1,24 +1,24 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const path = require('path');
-const express = require('express');
-const compression = require('compression');
-const favicon = require('serve-favicon');
-const mongoose = require('mongoose');
-const expressHandlebars = require('express-handlebars');
-const helmet = require('helmet');
-const session = require('express-session');
-const { RedisStore } = require('connect-redis');
-const redis = require('redis');
+const path = require("path");
+const express = require("express");
+const compression = require("compression");
+const favicon = require("serve-favicon");
+const mongoose = require("mongoose");
+const expressHandlebars = require("express-handlebars");
+const helmet = require("helmet");
+const session = require("express-session");
+const { RedisStore } = require("connect-redis");
+const redis = require("redis");
 
-const router = require('./router.js');
+const router = require("./router.js");
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/DomoMaker';
+const dbURI = process.env.MONGODB_URI || "mongodb://127.0.0.1/DomoMaker";
 mongoose.connect(dbURI).catch((err) => {
   if (err) {
-    console.log('Could not connect to database');
+    console.log("Could not connect to database");
     throw err;
   }
 });
@@ -27,8 +27,8 @@ const redisClient = redis.createClient({
   url: process.env.REDISCLOUD_URL,
 });
 
-redisClient.on('error', (err) => {
-  console.log('Redis Client Error', err);
+redisClient.on("error", (err) => {
+  console.log("Redis Client Error", err);
 });
 
 redisClient
@@ -37,7 +37,7 @@ redisClient
     const app = express();
 
     app.use(helmet());
-    app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted`)));
+    app.use("/assets", express.static(path.resolve(`${__dirname}/../hosted`)));
     app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
     app.use(compression());
     app.use(express.urlencoded({ extended: true }));
@@ -45,19 +45,19 @@ redisClient
 
     app.use(
       session({
-        key: 'sessionid',
+        key: "sessionid",
         store: new RedisStore({
           client: redisClient,
         }),
-        secret: 'Domo Arigato',
+        secret: "Domo Arigato",
         resave: false,
         saveUninitialized: false,
-      }),
+      })
     );
 
-    app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
-    app.set('view engine', 'handlebars');
-    app.set('views', `${__dirname}/../views`);
+    app.engine("handlebars", expressHandlebars.engine({ defaultLayout: "" }));
+    app.set("view engine", "handlebars");
+    app.set("views", `${__dirname}/../views`);
 
     router(app);
 
@@ -69,5 +69,5 @@ redisClient
     });
   })
   .catch((err) => {
-    console.log('Could not connect to Redis', err);
+    console.log("Could not connect to Redis", err);
   });
